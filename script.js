11279933f4d1b0ba42c6a0e683bd69897f6dcd18
AfrinaFarook <farooqafrina28@gmail.com>
2024-07-08 16:44:50 +0530

    hi

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log("welcome")
+
+//variables assigning
+
+let value=6;
+let second=8;
+let output=6+8
+//let output= value+second
+{
+    let value=7
+    console.log(value)
+}
+console.log("value of outside variable is "+ value)
